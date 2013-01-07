1b3031591404e2197d162d0753a661a98d4a039b
somewater <mktsz@mail.ru>
2013-01-07 16:25:15 +0400

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Himholod::Application.config.secret_token = '8642eda793a0cbe55eb263133fbe9589f7df575fc49a60460edc826f480fc4f4788e6151b531f9725b1c22a304337be0fe0889670d83394a261a96c8f446deea'
