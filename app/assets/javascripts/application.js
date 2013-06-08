d3280e1f288058dce0d0368d8b78be9bd8d05721
Pavel Naydenov <naydenov.p.v@gmail.com>
2013-06-09 03:12:42 +0400

    update jquery, bootstrap. Remove fancybox include

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require ./fancybox/jquery.fancybox
+//= require ./jquery
 //= require ./bootstrap/bootstrap
-//= #require_tree .
-
-// MENU SCRIPT
